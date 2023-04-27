a9a82c1a7f065af12ced9dc5447c36740e6abfd0
sathya <sathya123prakash@gmail.com>
2023-04-27 10:38:41 +0000

    3rdcommit

--- eaautomate.java
+++ eaautomate.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     
     int first = 11;
-    int second = 20;
+    int second = 21;
 
     // add two numbers
     int sum = first + second;
