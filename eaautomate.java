da30bbeca53e3e93aadab545a303149846fd529c
sathya <sathya123prakash@gmail.com>
2023-04-27 09:59:09 +0000

    2ndstatus

--- eaautomate.java
+++ eaautomate.java
@@ -2,11 +2,11 @@ class Main {
 
   public static void main(String[] args) {
     
-    int first = 10;
+    int first = 11;
     int second = 20;
 
     // add two numbers
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
-}
+
